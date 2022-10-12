4ed26230e72c2f9ebaf9f8e39c715fe5511988f8
yubinCloud <yubin_SkyWalker@yeah.net>
2022-10-12 16:15:10 +0800

    :bug: 加了个调试信息

--- src/.vuepress/sidebar/index.ts
+++ src/.vuepress/sidebar/index.ts
@@ -1,6 +1,7 @@
 import { sidebar } from "vuepress-theme-hope";
 import { getKgNavBar } from "../navbar";
 
+console.log(getKgNavBar());
 export const mySidebar = sidebar({
     "/KG/": getKgNavBar(),
 });
