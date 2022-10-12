bdf56eab2e78abd30cbd0f86c3155ed0f719ae23
yubinCloud <yubin_SkyWalker@yeah.net>
2022-10-12 14:11:23 +0800

    :construction_worker: 可以自动识别 knowledge-graph 目录下面的文件，并自动生成 navbar 了

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -1,20 +1,41 @@
 import { navbar } from "vuepress-theme-hope";
+import * as fs from "fs";
 
-const krlNavBar = navbar([
-  {
-    text: "TransE",
-    icon: "edit",
-    link: "transe/"
-  },
-  "rescal-and-extensions"  
-])
 
+
+function cutoffMdSuffix(fileName: string): string | null {
+  if (!fileName.endsWith('.md'))
+    return null;
+  const suffixIdx = fileName.lastIndexOf('.');
+  return fileName.substring(0, suffixIdx);
+}
+
+/**
+ * 获取 knowledge-graph 下面的所有文件的 navbar
+ */
+const getKgNavBar = () => {
+  const kgNavBar = navbar([]);
+  const kgBlogs: string[] = [];
+  fs.readdir('src/knowledge-graph', function (err, files) {
+    if (err) {
+      throw err;
+    }
+    for (let fileName of files) {
+      const blogName = cutoffMdSuffix(fileName);
+      if (blogName != null) {
+        kgBlogs.push(blogName);
+      }
+    }
+    console.log(kgBlogs);
+  })
+  return navbar(kgBlogs);
+}
 
 export const my_navbar = navbar([
   {
     text: "knowledge-graph",
-    prefix: "/kg/",
+    prefix: "/knowledge-graph/",
     icon: "discover",
-    children: krlNavBar,
+    children: getKgNavBar()
   },
 ]);
